6e76f8281191649d470c1363304b83ca73d03ae3
Ben Fogarty <ben.t.fogarty@gmail.com>
2020-03-21 22:57:50 -0500

    separated response into new struct

--- src/qrmodel.jl
+++ src/qrmodel.jl
@@ -1,6 +1,19 @@
 using DataFrames, StatsBase, StatsModels, SparseArrays
 
 """
+    QuantRegResp
+
+Contains response from fitting a quantile regression model.
+"""
+mutable struct QuantRegResp
+    fitted::Bool
+    coefficients::Union{Vector, Nothing}
+    residuals::Union{Vector, Nothing}
+    dual::Union{Vector, Nothing}
+    fittedvalues::Union{Vector, Nothing}
+end
+
+"""
     QuantRegModel(formula, data; τ=0.5, method="br")
 
 Quantile Regression model at the τth percentile fitting `data` according to `formula` using
@@ -15,20 +28,16 @@ struct QuantRegModel <: StatisticalModel
     formula::FormulaTerm
     data::DataFrame
     mf::ModelFrame
+    mm::ModelMatrix
     τ::Number
     method::String
-    y::Vector
-    X::Array
-    fitted::Bool
-    coefficients::Union{Vector, Nothing}
-    residuals::Union{Vector, Nothing}
-    dual::Union{Vector, Nothing}
-    fittedvalues::Union{Vector, Nothing}
+    response::QuantRegResp
 end
 
 # should this be convenience constructor or function rq
 function QuantRegModel(formula, data; τ::Number=0.5, method::String="br")
     mf = ModelFrame(formula, data)
-    QuantRegModel(formula, data, mf, τ, method, response(mf), ModelMatrix(mf).m, false, 
-                  nothing, nothing, nothing, nothing)
+    mm = ModelMatrix(mf)
+    mr = QuantRegResp(false, nothing, nothing, nothing, nothing)
+    QuantRegModel(formula, data, mf,mm, τ, method, mr)
 end
